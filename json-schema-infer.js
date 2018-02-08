c77b2a8cf8fb563287fc1baac3f57295ad0f087e
alviser <alviser@users.noreply.github.com>
2018-02-08 15:26:44 +0100

    This project starts here

---
+++ json-schema-infer.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var myJSONstring = '{"name":"test","parameter":2,"collection":[0,2],"dizionario":{"voceA":1,"voceB":2}}';
+var s = JSON.parse(myJSONstring);
+
+/*
+  typeValue(value)
+  returns a string representing the type of value as recognized by
+  the typeof function with some more types added
+  in case of dicitionaries it returns again a dictionary
+  typing all its keys' values
+*/
+
+function typeValue(val) {
+  if (Array.isArray(val)) {
+    return 'array';
+  }
+  
+  if (typeof val == 'object') {
+    return getProperties(val);
+  }
+  
+  return typeof val;
+}
+
+/*
+  getProperties(JSONobject)
+  returns a typed properties list composed by the same keys with
+  values replaced with their types using the typeValue function
+  in case of contained dictionaries it gets called recursively by
+  the typeValue(val)
+*/
+
+function getProperties(j) {
+  var k = Object.keys(j);
+  
+  k.forEach(function(name) {
+    j[name] = typeValue(j[name]);
+  })
+  
+  return j;
+}
+
+
+function getSchema(json_object) {
+  var schema = {};
+
+  schema['$schema'] = 'http://json-schema.org/draft-06/schema#';
+  schema['title'] = 'JSON inferred schema';
+  schema['description'] = 'JSON inferred schema';
+  schema['type']  = 'object';
+  schema['properties'] = getProperties(json_object);
+  schema['required'] = Object.keys(schema['properties']);
+
+  return schema;
+}
+
+console.log(getSchema(s));
